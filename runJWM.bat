8a6c667e6c5a4d8c2459e510e582aa22ca1efed2
phoeniix <webmaster@phoeniix.com>
2015-05-26 09:06:12 -0700

    adding supporting files

---
+++ runJWM.bat
@@ -0,0 +1,6 @@
+echo off
+rem JavaWebMonitor (JWM)
+rem http://forum.phoeniix.com/
+
+start /MIN java -jar JWebMonitor.jar "config.xml"
+rem pause
---
+++ runJWM.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Java Web Monitor (JWM)
+# http://forum.phoeniix.com/
+#
+/opt/jre1.5.0_06/bin/java -jar JWebMonitor.jar "config.xml"
+#/usr/bin/java -jar JWebMonitor.jar "config.xml"
+#sleep 1
